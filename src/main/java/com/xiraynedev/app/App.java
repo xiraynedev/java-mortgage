0c9b1e124ea1c0559a37f19f818b90c7ae6754f2
xiraynedev <xiraynedev@gmail.com>
2022-12-24 15:02:36 -0500

    refactor application code

--- src/main/java/com/xiraynedev/app/App.java
+++ src/main/java/com/xiraynedev/app/App.java
@@ -2,6 +2,7 @@ package com.xiraynedev.app;
 
 public class App {
     public static void main(String[] args) {
-        new MortgageCalculator();
+        MortgageCalculator mortgageCalculator = new MortgageCalculator();
+        mortgageCalculator.startCalculator();
     }
 }
